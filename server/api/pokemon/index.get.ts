ced256b7a6f4be27b3d27894ed8fc5e6f00f838e
lewis-cortright <legend_c_234@hotmail.com>
2026-06-08 08:51:54 -0600

    fix(landing): update offset param

--- server/api/pokemon/index.get.ts
+++ server/api/pokemon/index.get.ts
@@ -7,7 +7,10 @@ import type { H3Event } from "h3";
 
 
 export default defineEventHandler(async (event: H3Event): Promise<PokemonDetails[]> => {
-  const listResponse: PokemonPageData = await getPokemonList(event, 60);
+  const query = getQuery(event);
+  const limit = parseInt(query.limit?.toString() ?? '60', 10);
+  const offset = parseInt(query.offset?.toString() ?? '0', 10);
+  const listResponse: PokemonPageData = await getPokemonList(event, limit, offset);
 
   const pokemonProfileList: PokemonApiResponse[] = await Promise.all(
     listResponse.results.map((pokemonListItem) => getPokemonByUrl(pokemonListItem.url))
